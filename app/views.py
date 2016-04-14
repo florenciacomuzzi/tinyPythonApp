d5ec1a400732a04f37751ecf68088c565ac63133
Flo Sal <florenciabsalinas@gmail.com>
2016-04-13 20:36:53 -0400

    follow pep8, add debugger to config

--- app/views.py
+++ app/views.py
@@ -1,8 +1,24 @@
+import datetime 
+
+from flask import render_template
+
+
 from . import app
+
+
 
 
 @app.route('/')
 def hello():
     return 'Hello World'
 
+@app.route('/time') #this line makes this a view(instead of just a fct) by creating a path
+def get_time():
+    now = datetime.datetime.now()
+    return now.strftime('%Y-%m-%d %H:%M:%S')
+
+@app.route('/pretty')
+def pretty():
+    context = {'who': 'Flo'}
+    return render_template('pretty.html', **context)
 
